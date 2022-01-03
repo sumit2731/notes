e2499471fecdd40f6291144a7194db91ff6a3199
Sumeet Sood <ssood@gainsight.com>
2022-01-03 22:01:19 +0530

    pharmasy

--- Lectures/JavaScript Algorithms and Data Structures Masterclass/6)OptionalCodingChallenges/2)multiplePointers/app.js
+++ Lectures/JavaScript Algorithms and Data Structures Masterclass/6)OptionalCodingChallenges/2)multiplePointers/app.js
@@ -1,27 +1,13 @@
-const Deque = require('./collections/deque'); //http://www.collectionsjs.com
-
-
-function find_subarrays(arr, target) {
-  let result = [],
-    product = 1,
-    left = 0;
-    //n
-  for (right = 0; right < arr.length; right++) {
-    product *= arr[right];
-    //n
-    while ((product >= target && left < arr.length)) {
-      product /= arr[left];
-      left += 1;
-    }
-    // since the product of all numbers from left to right is less than the target therefore,
-    // all subarrays from left to right will have a product less than the target too; to avoid
-    // duplicates, we will start with a subarray containing only arr[right] and then extend it
-    const tempList = new Deque();
-    //n
-    for (let i = right; i > left - 1; i--) {
-      tempList.unshift(arr[i]);
-      result.push(tempList.toArray());
+const remove_duplicates = function(arr) {
+  // TODO: Write your code here
+  let lastUniqueIndex = 0;
+  for(let i =1;i<=arr.length; i++) {
+    if(arr[i] !== arr[i-1]) {
+      lastUniqueIndex++;
+      arr[lastUniqueIndex] = arr[i];
     }
   }
-  return result;
-}
+  return lastUniqueIndex++;
+};
+
+console.log(remove_duplicates([2,3,3,3,6,9,9]));
