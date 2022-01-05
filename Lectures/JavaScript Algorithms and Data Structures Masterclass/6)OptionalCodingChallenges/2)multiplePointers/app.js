3c15c3b5e80254cccbb09d795dd9dd93b9067dbf
Sumeet Sood <ssood@gainsight.com>
2022-01-05 19:14:12 +0530

    educative notes

--- Lectures/JavaScript Algorithms and Data Structures Masterclass/6)OptionalCodingChallenges/2)multiplePointers/app.js
+++ Lectures/JavaScript Algorithms and Data Structures Masterclass/6)OptionalCodingChallenges/2)multiplePointers/app.js
@@ -1,13 +1,29 @@
-const remove_duplicates = function(arr) {
+const search_triplets = function(arr) {
+  triplets = [];
   // TODO: Write your code here
-  let lastUniqueIndex = 0;
-  for(let i =1;i<=arr.length; i++) {
-    if(arr[i] !== arr[i-1]) {
-      lastUniqueIndex++;
-      arr[lastUniqueIndex] = arr[i];
+  arr = arr.sort((num1 , num2) => num1-num2);
+  for(let i = 0; i<=arr.length-3; i++) {
+    if((i > 0) && arr[i] === arr[i-1]) continue;
+    let requiredSum = (-1) * (arr[i]);
+    let start = i+1, end = arr.length-1;
+    while(end > start) {
+      let currentSum = arr[start] + arr[end];
+      if(currentSum === requiredSum) {
+        triplets.push([arr[i],arr[start],arr[end]]);
+        start++;
+        end--;
+        while(arr[start] === arr[start-1]) start++;
+        while(arr[end] === arr[end+1]) end--;
+      } else if(currentSum > requiredSum) {
+        end--;
+      }
+      else {
+        start++;
+      }
     }
   }
-  return lastUniqueIndex++;
+  return triplets;
 };
 
-console.log(remove_duplicates([2,3,3,3,6,9,9]));
+//console.log(search_triplets([-3,0,1,2,-1,1,-2]));
+console.log(search_triplets([-3,0,1,2,2,2,2,-1,1,1,1-2]));
