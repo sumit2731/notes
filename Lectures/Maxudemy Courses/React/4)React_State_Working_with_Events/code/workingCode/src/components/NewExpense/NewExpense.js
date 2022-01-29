aa90491dccb832f3970e4ba8b7ef913a7fbc4511
Sumeet Sood <sumitsood3127@gmal.com>
2022-01-30 03:18:19 +0530

    React Course

---
+++ Lectures/Maxudemy Courses/React/4)React_State_Working_with_Events/code/workingCode/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,19 @@
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {id: Math.random().toString() ,...enteredExpenseData};
+        props.onAddExpense(expenseData);
+    };
+    
+    return (
+        <div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
+}
+
+export default NewExpense;
+
