67bd0da6e81bb09882f434bd98e39a35f6ea0e17
Sumeet Sood <sumitsood3127@gmal.com>
2022-02-20 21:08:11 +0530

    Section 16 Part2

--- Lectures/Maxudemy Courses/React/16)workingWithForms/code/current/src/components/SimpleInput.js
+++ Lectures/Maxudemy Courses/React/16)workingWithForms/code/current/src/components/SimpleInput.js
@@ -1,35 +1,64 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName , setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  //const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  //const [formIsValid, setFormIsValid] = useState(false);
+
+  
+  
+  const enteredNameIsValid  = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
+  /**
+   * @Desc here check all inputs, in this case we have only one input
+   */
+  if(enteredNameIsValid) {
+    formIsValid = true;
+  } else {
+    formIsValid = false;
+  }
+
+  /**
+   * @Desc we validate validity fo form in useEffect as value of this state depends upon value of other state
+   *  here dependecies will be all inputs in form , here we have only one.
+   * 
+   * @Desc This code is commneted out because we do not need to manage a separate state, we can just use a varibel for this.
+   */
+  // useEffect(() => {
+  //   if(enteredNameIsValid) {
+  //     setFormIsValid(true);
+  //   } else {
+  //     setFormIsValid(false);
+  //   }
+  // },[enteredNameIsValid]);
+  
   const nameInputChnageHandler = event => {
     setEnteredName(event.target.value);
-    if(event.target.value.trim() !== '') {
-      setEnteredNameIsValid(true);
-    }
-    //setEnteredNameTouched(true);
+    // if(event.target.value.trim() !== '') {
+    //   setEnteredNameIsValid(true);
+    // }
   }
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
+    // if(enteredName.trim() === '') {
+    //   setEnteredNameIsValid(false);
+    // }
   };
 
   const formSubmissionHandler = event => {
     event.preventDefault();
     setEnteredNameTouched(true);
-    if(enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if(!enteredNameIsValid) {
       return ;
     }
-    setEnteredNameIsValid(true);
+    setEnteredName('');
+    setEnteredNameTouched(false);
     console.log(enteredName);
   }
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  
   const nameInputClasses = (nameInputIsInvalid) ? `form-control invalid` : `form-control`;
   return (
     <form onSubmit = {formSubmissionHandler}>
@@ -39,7 +68,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvalid && <p className ="error-text" >Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
