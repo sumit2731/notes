030a10c61e2a2841cc341d7997620dbb4313ec12
Sumeet Sood <sumitsood3127@gmal.com>
2022-02-20 23:06:18 +0530

    Section 16 Part 4

--- Lectures/Maxudemy Courses/React/16)workingWithForms/code/current/src/components/SimpleInput.js
+++ Lectures/Maxudemy Courses/React/16)workingWithForms/code/current/src/components/SimpleInput.js
@@ -1,21 +1,38 @@
 import {useEffect, useState} from 'react';
+import useInput from '../hooks/use-input';
 
 const SimpleInput = (props) => {
-  const [enteredName , setEnteredName] = useState('');
-  //const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  //const [formIsValid, setFormIsValid] = useState(false);
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError, 
+    valueChangeHandler:nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput(value => (value.trim() !== ''));
+  
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError, 
+    valueChangeHandler:emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput
+  } = useInput(value => value && value.indexOf('@') !== -1);
+  
+  // const [enteredName , setEnteredName] = useState('');
+  // const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+  //const [enteredEmail, setEnteredEmail] = useState('');
+  //const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
   
   
-  const enteredNameIsValid  = enteredName.trim() !== '';
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  // const enteredNameIsValid  = enteredName.trim() !== '';
+  // const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
   
-  const enteredEmailIsValid = enteredEmail && enteredEmail.indexOf('@') !== -1;
-  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+  //const enteredEmailIsValid = enteredEmail && enteredEmail.indexOf('@') !== -1;
+  //const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
   
   let formIsValid = false;
   /**
@@ -41,58 +58,57 @@ const SimpleInput = (props) => {
   //   }
   // },[enteredNameIsValid]);
   
-  const nameInputChnageHandler = event => {
-    setEnteredName(event.target.value);
-    // if(event.target.value.trim() !== '') {
-    //   setEnteredNameIsValid(true);
-    // }
-  }
+  
+  // const nameInputChnageHandler = event => {
+  //   setEnteredName(event.target.value);
+  // }
 
-  const emailInputChangeHandler = event => {
-    setEnteredEmail(event.target.value);
-  };
+  // const emailInputChangeHandler = event => {
+  //   setEnteredEmail(event.target.value);
+  // };
 
-  const nameInputBlurHandler = (event) => {
-    setEnteredNameTouched(true);
-    // if(enteredName.trim() === '') {
-    //   setEnteredNameIsValid(false);
-    // }
-  };
+  // const nameInputBlurHandler = (event) => {
+  //   setEnteredNameTouched(true);
+  // };
 
-  const emailInputBlurHandler = (event) => {
-    setEnteredEmailTouched(true);
-  }
+  // const emailInputBlurHandler = (event) => {
+  //   setEnteredEmailTouched(true);
+  // }
 
   const formSubmissionHandler = event => {
     event.preventDefault();
-    setEnteredNameTouched(true);
+    //setEnteredNameTouched(true);
     if(!enteredNameIsValid) {
       return ;
     }
     
-    setEnteredName('');
-    setEnteredNameTouched(false);
+    // setEnteredName('');
+    // setEnteredNameTouched(false);
+
+    resetNameInput();
     
-    setEnteredEmail('');
-    setEnteredEmailTouched(false);
+    // setEnteredEmail('');
+    // setEnteredEmailTouched(false);
+
+    resetEmailInput();
     console.log(enteredName);
   }
   
-  const nameInputClasses = (nameInputIsInvalid) ? `form-control invalid` : `form-control`;
-  const emailInputClasses = (emailInputIsInvalid) ? `form-control invalid` : `form-control`;
+  const nameInputClasses = (nameInputHasError) ? `form-control invalid` : `form-control`;
+  const emailInputClasses = (emailInputHasError) ? `form-control invalid` : `form-control`;
   return (
     <form onSubmit = {formSubmissionHandler}>
       
       <div className= {nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange = {nameInputChnageHandler} onBlur = {nameInputBlurHandler} value ={enteredName} />
-        {nameInputIsInvalid && <p className ="error-text" >Name must not be empty</p>}
+        <input type='text' id='name' onChange = {nameChangeHandler} onBlur = {nameBlurHandler} value ={enteredName} />
+        {nameInputHasError && <p className ="error-text" >Name must not be empty</p>}
       </div>
 
       <div className= {emailInputClasses}>
         <label htmlFor='email'>Your Email</label>
-        <input type='email' id='email' onChange = {emailInputChangeHandler} onBlur = {emailInputBlurHandler} value = {enteredEmail} />
-        {emailInputIsInvalid && <p className ="error-text" >Please Enter a Valid Email</p>}
+        <input type='email' id='email' onChange = {emailChangeHandler} onBlur = {emailBlurHandler} value = {enteredEmail} />
+        {emailInputHasError && <p className ="error-text" >Please Enter a Valid Email</p>}
       </div>
       
       <div className="form-actions">
---
+++ Lectures/Maxudemy Courses/React/16)workingWithForms/code/current/src/hooks/use-input.js
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+
+const useInput = (validateValue) => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isTouched, setIsTouched] = useState(false);
+
+    const valueIsValid = validateValue(enteredValue);
+    const hasError = isTouched && !valueIsValid;
+
+    const valueChangeHandler = event => {
+        setEnteredValue(event.target.value);
+    };
+        
+    const inputBlurHandler = (event) => {
+        setIsTouched(true);
+    }
+
+    const reset = () => {
+        setEnteredValue('');
+        setIsTouched(false);
+    }
+
+    return {
+        value: enteredValue,
+        isValid:valueIsValid,
+        hasError, 
+        inputBlurHandler ,
+        valueChangeHandler,
+        reset
+    }
+}
+
+export default useInput;
