42c8c6c8890285daaf04030d2f901181f0c38aac
Sumeet Sood <sumitsood3127@gmal.com>
2022-02-24 19:00:14 +0530

    Section 20 Commit 1 End

--- Lectures/Maxudemy Courses/React/20)Routing/code/working/src/App.js
+++ Lectures/Maxudemy Courses/React/20)Routing/code/working/src/App.js
@@ -1,7 +1,42 @@
+import { Switch, Route,Redirect } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail'
+import MainHeader from './components/MainHeader';
+
 function App() {
+  console.log("App Component rendered");
   return (
     <div>
-      <h2>Let's get started!</h2>
+
+      <MainHeader></MainHeader>
+
+      <main>
+
+        <Switch>
+
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
+          
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+
+          <Route path="/products/:productId">
+            <ProductDetail></ProductDetail>
+          </Route>
+        
+        </Switch>
+
+      </main>
+
+
+
     </div>
   );
 }
---
+++ Lectures/Maxudemy Courses/React/20)Routing/code/working/src/components/MainHeader.js
@@ -0,0 +1,25 @@
+import {Link, NavLink} from 'react-router-dom';
+import classes from './MainHeader.module.css';
+
+const Mainheader = () => {
+    return (
+        <header className = {classes.header}>
+            <nav>
+                <ul>
+                    
+                    <li>
+                       
+                        <NavLink to="/welcome" activeClassName = {classes.active}>Welcome</NavLink>
+                    </li>
+
+                    <li>
+                  
+                        <NavLink to="/products" activeClassName = {classes.active}>Products</NavLink>
+                    </li>
+                
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Mainheader;
