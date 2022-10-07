1160a82abbe88fa4ea73d61ab7f3f9940d531541
sumeet-sood27 <sumeet.sood@freshworks.com>
2022-10-07 09:21:03 +0530

    Added Code

---
+++ Lectures/ohnas Course/Advanced React Patterns/2)The Medium Clap/code1/advanced-react-patterns-ultrasimplified/showcase/src/patterns/02.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.css";
+
+const MediumClap = () => {
+  return <button className={styles.clap2}>Hello</button>;
+};
+
+export default MediumClap;
