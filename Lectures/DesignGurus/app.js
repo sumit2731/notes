b9134cc0213f104b216ecd5c7c612c7b7a41661c
Sumeet Sood <sumitsood3127@gmail.com>
2023-06-17 13:48:37 +0530

    Josh CSS Lectures

---
+++ Lectures/DesignGurus/app.js
@@ -0,0 +1,12 @@
+const max_sub_array_of_size_k = function(k, arr) {
+    let maxSum = Infinity, currentPointer = 0;
+    while(currentPointer < arr.length) {
+        
+    }
+  };
+  
+  console.log(
+      `Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])}`);
+  console.log(
+      `Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])}`);
+  
