a43390bb393a92a9c8862c30db356aa5be73a0bb
Sumeet Sood <sumitsood3127@gmail.com>
2023-06-23 19:42:43 +0530

    DOM

--- Lectures/Topic Notes/1_Javascript/JavascriptInfo/app.js
+++ Lectures/Topic Notes/1_Javascript/JavascriptInfo/app.js
@@ -1,20 +1,65 @@
-let element = document.getElementById('sumit');
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect();
 
-let observer = new MutationObserver((mutationRecords) => {
-  console.log(mutationRecords);
-});
+  return {
+    top: box.top + window.pageYOffset,
+    left: box.left + window.pageXOffset
+  };
+}
 
-observer.observe(element,{
-  childList: true, // observe direct children
-  subtree: true, // and lower descendants too
-  characterDataOldValue: true // pass old data to callback
-});
+function showNote(anchor, position, html) {
+
+  let note = document.createElement('div');
+  note.className = "note";
+  note.innerHTML = html;
+  document.body.append(note);
+
+  positionAt(anchor, position, note);
+}
+
+function positionAt(anchor, position, elem) {
+
+  let anchorCoords = getCoords(anchor);
+
+  switch (position) {
+    case "top-out":
+      elem.style.left = anchorCoords.left + "px";
+      elem.style.top = anchorCoords.top - elem.offsetHeight + "px";
+      break;
+
+    case "right-out":
+      elem.style.left = anchorCoords.left + anchor.offsetWidth + "px";
+      elem.style.top = anchorCoords.top + "px";
+      break;
+
+    case "bottom-out":
+      elem.style.left = anchorCoords.left + "px";
+      elem.style.top = anchorCoords.top + anchor.offsetHeight + "px";
+      break;
+
+    case "top-in":
+      elem.style.left = anchorCoords.left + "px";
+      elem.style.top = anchorCoords.top + "px";
+      break;
+
+    case "right-in":
+      elem.style.width = '150px';
+      elem.style.left = anchorCoords.left + anchor.offsetWidth - elem.offsetWidth + "px";
+      elem.style.top = anchorCoords.top + "px";
+      break;
+
+    case "bottom-in":
+      elem.style.left = anchorCoords.left + "px";
+      elem.style.top = anchorCoords.top + anchor.offsetHeight - elem.offsetHeight + "px";
+      break;
+  }
+
+}
 
 
-let span = document.createElement('span');
-span.id = 'span1';
-element.append(span);
+let blockquote = document.querySelector('blockquote');
 
-let span2 = document.createElement('span');
-span2.id = 'span2';
-span.append(span2);
+showNote(blockquote, "top-in", "note top-in");
+showNote(blockquote, "top-out", "note top-out");
+showNote(blockquote, "right-out", "note right-out");
+showNote(blockquote, "bottom-in", "note bottom-in");
