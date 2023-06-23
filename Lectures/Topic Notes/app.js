c7d25229b638685ffb743f6212b7fcf9a4a72028
Sumeet Sood <sumitsood3127@gmail.com>
2023-06-23 17:48:11 +0530

    Completed Section 1 of DOM manipulation

--- Lectures/Topic Notes/app.js
+++ Lectures/Topic Notes/app.js
@@ -1,7 +1,34 @@
-sumeet.addEventListener('click', (event) => {
-  console.log("Hello");
-  event.target.style = {
-    color: 'green'
+let elem = document.getElementById("coords-show-mark");
+
+
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset
+    };
   }
-  // event.target.style.color = 'green';
-});
+
+
+function createMessageUnder(elem, html) {
+  let message = document.createElement('div');
+  
+  message.style.cssText = "position:absolute; color: red";
+  //let coords = elem.getBoundingClientRect();
+  
+  let coords = getCoords(elem);
+  
+  
+  message.style.left = coords.left + "px";
+  message.style.top = coords.bottom + "px";
+  message.innerHTML = html;
+  return message;
+}
+
+// Usage:
+// add it for 5 seconds in the document
+let message = createMessageUnder(elem, 'Hello, world!');
+document.body.append(message);
