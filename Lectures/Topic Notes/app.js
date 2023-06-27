214fbfd3f764b62ebeaebc0a190648b99b22a7f9
Sumeet Sood <sumitsood3127@gmail.com>
2023-06-27 08:25:56 +0530

    Custom Events

--- Lectures/Topic Notes/app.js
+++ Lectures/Topic Notes/app.js
@@ -1,34 +1,9 @@
-let elem = document.getElementById("coords-show-mark");
+elem.oncontextmenu = function(event) {
+  event.preventDefault();
+  alert("Button context menu");
+};
 
-
-function getCoords(elem) {
-    let box = elem.getBoundingClientRect();
-  
-    return {
-      top: box.top + window.pageYOffset,
-      right: box.right + window.pageXOffset,
-      bottom: box.bottom + window.pageYOffset,
-      left: box.left + window.pageXOffset
-    };
-  }
-
-
-function createMessageUnder(elem, html) {
-  let message = document.createElement('div');
-  
-  message.style.cssText = "position:absolute; color: red";
-  //let coords = elem.getBoundingClientRect();
-  
-  let coords = getCoords(elem);
-  
-  
-  message.style.left = coords.left + "px";
-  message.style.top = coords.bottom + "px";
-  message.innerHTML = html;
-  return message;
-}
-
-// Usage:
-// add it for 5 seconds in the document
-let message = createMessageUnder(elem, 'Hello, world!');
-document.body.append(message);
+document.oncontextmenu = function(event) {
+  event.preventDefault();
+  alert("Document context menu");
+};
