afe5d548248124a4a949bed1d52eb18ce560dd02
Sumeet Sood <sumeet.sood@perforce.com>
2023-11-27 22:25:29 +0530

    Added new code

---
+++ Lectures/Maxudemy Courses/ReactWithTypeScript/6_sideEffects_useEffect_dataFetching/notes.ts
@@ -0,0 +1,7 @@
+/**
+ * @useEffect and readONlyError with useref
+ *
+ * here we added the useEffect hook. there is nothing much with it in typescript.
+ *
+ * see src/components/Timer.tsx for details
+ */
