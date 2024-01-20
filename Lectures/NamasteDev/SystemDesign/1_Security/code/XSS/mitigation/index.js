a45cab6b79d451452a3c1499423c5ec33058c116
Sumeet Sood <sumeet.sood@perforce.com>
2024-01-20 20:24:13 +0530

    Secod Secuirty Module

--- Lectures/NamasteDev/SystemDesign/1_Security/code/XSS/mitigation/index.js
+++ Lectures/NamasteDev/SystemDesign/1_Security/code/XSS/mitigation/index.js
@@ -4,21 +4,60 @@ const PORT = 3010;
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader(
-        'Content-Security-Policy',
-        "default-src 'self';" + 
-        "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;"
-    );
-    next();
-})
+  /**
+   * This says default-src for all content from my origin only
+   * default-src for all the content is my origin
+   */
+  //   res.setHeader(
+  //     "Content-Security-Policy",
+  //     "default-src 'self';"
+  //   );
 
-app.use(express.static('public'));
+  /**
+   * here we added a url from where only script elements can be loaded
+   */
+  //   res.setHeader(
+  //     "Content-Security-Policy",
+  //     "default-src 'self';" + "script-src 'self' http://unsecure.com"
+  //   );
 
-app.get('/', (req, res) => {
-    console.log(req.url);
-    res.sendFile(__dirname + '/index.html');
+  /**
+   * Allows the execution of inline script also
+   */
+  //   res.setHeader(
+  //     "Content-Security-Policy",
+  //     "default-src 'self';" +
+  //       "script-src 'self' 'unsafe-inline' http://unsecure.com"
+  //   );
+  /**
+   * Allows the execution of inline scripts with specififc nounce only
+   * scripts that we want to execute needs to have same nounce attribute.
+   * nounce attribute is not shown in browser dev tools
+   */
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self';" +
+      "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com"
+  );
+  next();
+});
+
+// app.use((req, res, next) => {
+//     res.setHeader(
+//         'Content-Security-Policy',
+//         "default-src 'self';" +
+//         "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;"
+//     );
+//     next();
+// })
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started at http://locolhost:${PORT}`);
+  console.log(`Server started at http://locolhost:${PORT}`);
 });
