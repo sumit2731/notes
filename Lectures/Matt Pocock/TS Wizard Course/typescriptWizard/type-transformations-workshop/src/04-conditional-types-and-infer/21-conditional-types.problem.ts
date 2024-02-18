d7a1da191fe427a469d1cb744f1b7d2dd7ef6a17
Sumeet Sood <sumeet.sood@perforce.com>
2024-02-18 12:48:29 +0530

    Started With Generics

--- Lectures/Matt Pocock/TS Wizard Course/typescriptWizard/type-transformations-workshop/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++
@@ -1,8 +0,0 @@
-import { Equal, Expect } from "../helpers/type-utils";
-
-type YouSayGoodbyeAndISayHello = unknown;
-
-type tests = [
-  Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
-];
--- Lectures/Matt Pocock/TS Wizard Course/typescriptWizard/type-transformations-workshop/src/04-conditional-types-and-infer/28-distributive-conditional-types.solution.2.ts
+++
@@ -1,9 +0,0 @@
-import { Equal, Expect } from "../helpers/type-utils";
-
-type Fruit = "apple" | "banana" | "orange";
-
-type GetAppleOrBanana<T> = T extends "apple" | "banana" ? T : never;
-
-type AppleOrBanana = GetAppleOrBanana<Fruit>;
-
-type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
