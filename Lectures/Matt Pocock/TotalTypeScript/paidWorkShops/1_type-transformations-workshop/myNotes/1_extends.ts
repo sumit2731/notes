df54f7254d6886e45a22e3907c2bb5d2793b674a
Sumeet Sood <sumeet.sood@perforce.com>
2024-02-24 19:17:08 +0530

    Added Worker Code

--- Lectures/Matt Pocock/TotalTypeScript/paidWorkShops/1_type-transformations-workshop/myNotes/1_extends.ts
+++ Lectures/Matt Pocock/TotalTypeScript/paidWorkShops/1_type-transformations-workshop/myNotes/1_extends.ts
@@ -1,21 +1,69 @@
 /**
- * 1)see definiition of extends in conditional types docs.
+ * In TSdocs of Conditional Types
+ * typeA extends typeB
  * 
- * 2)how extends works in case of union type, see example in mapped type documentation.
- *  when we say Union typ extends someType, that means each member of UnionType extends that type
+ * =>  type on the left of the extends is assignable to the one on the right,
+ */
+
+
+/**
+ * objectA extends objectB => means objectA has all properties of objectB, plus some extra
+ * 
  */
 
 /**
- * here employee extends perosn
+ * here employee extends person
  */
 let person = {
-    name: 'sumeet'
-  }
+  name: 'sumeet'
+}
   
-  let employee = {
-    name: 'sumeet',
-    id:1
-  }
-  
-  person = employee;
-  person.name
+let employee = {
+  name: 'sumeet',
+  id:1
+}
+
+person = employee;
+person.name
+
+/**
+ * Union extends TypeA
+ * 
+ * That mean each member of Union can be assigned to TypeA
+ * 
+ *  a)see example in mapped type documentation.
+ *  b)see definition of Pick
+ */
+
+//ex1 - Each member of Union will hve kind Property
+type EventConfig<Events extends { kind: string }> = {
+  [E in Events as E["kind"]]: (event: E) => void;
+}
+
+type SquareEvent = { kind: "square", x: number, y: number };
+type CircleEvent = { kind: "circle", radius: number };
+
+type Config = EventConfig<SquareEvent | CircleEvent>
+
+//here we make sure each member of Union is a proeprty of passed Object
+
+/**
+ * ex2 - Each member of Union is memeber of another union - 
+ *  unionA extends UnionB (means each of UnionA is present in UNionB)
+ * a|b extends a | b | c
+ */
+
+type Pick2<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+
+/**
+ * ex 3 - how to make sure only a union type is passed
+ */
+
+type Record2<K extends keyof any, T> = {
+  [P in K]: T;
+};
+
+type Omit3<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
