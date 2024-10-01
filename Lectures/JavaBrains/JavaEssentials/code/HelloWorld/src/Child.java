89a482af846ee3aeb4b45f440c7497eeb1d6a186
Sumeet Sood <sumeet.sood@perforce.com>
2024-10-01 18:04:06 +0530

    correct some notes

---
+++ Lectures/JavaBrains/JavaEssentials/code/HelloWorld/src/Child.java
@@ -0,0 +1,6 @@
+public class Child extends Parent{
+    @Override
+    public int getName(String s1) {
+        return 1;
+    }
+}
---
+++ Lectures/JavaBrains/JavaEssentials/code/HelloWorld/src/Parent.java
@@ -0,0 +1,5 @@
+public class Parent {
+    public String getName() {
+        return "sumeet";
+    }
+}
