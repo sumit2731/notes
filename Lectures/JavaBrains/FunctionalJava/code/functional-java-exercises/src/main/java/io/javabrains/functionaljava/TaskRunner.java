b5c2b5379dd981d4d1bca18f7bde83171bfb8181
Sumeet Sood <sumeet.sood@perforce.com>
2024-10-01 23:34:07 +0530

    Started with Functional programming

---
+++ Lectures/JavaBrains/FunctionalJava/code/functional-java-exercises/src/main/java/io/javabrains/functionaljava/TaskRunner.java
@@ -0,0 +1,30 @@
+package io.javabrains.functionaljava;
+
+import java.time.LocalDateTime;
+
+public class TaskRunner {
+    public static void runner(Task task) {
+        System.out.println("Start: " + LocalDateTime.now());
+        task.run();
+        System.out.println("End: " + LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        Task task = new HelloWorldTask();
+        TaskRunner.runner(task);
+    }
+}
+
+interface Task {
+    void run();
+}
+
+class HelloWorldTask implements Task {
+
+    @Override
+    public void run() {
+        System.out.println("Hello World");
+    }
+}
+
+myFunc = () -> System.out.println("Hello World");
