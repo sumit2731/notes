4d7d270493348e686e4ba1d26fb594f13a22ad89
Sumeet Sood <sumeet.sood@perforce.com>
2024-10-31 13:05:51 +0530

    chnaged Folder Name

---
+++ Lectures/Matt Pocock/TotalTypeScript/paidWorkShops/1_pro-essentials-workshop/myNotes/1_kickStartYourTsSetUp.ts
@@ -0,0 +1,9 @@
+/**
+ * 2_JS vs Ts in build process - see figure 1 for feedback loop. IDE has a TSserver that gives
+ *  feedback loop.
+ *
+ * 3  _ things needed for TS setup -
+ *  IDE
+ *  nodejs - ts cli is written in ts , so we need nodejs to run it.
+ *  execution env - nodejs or chrome
+ */
