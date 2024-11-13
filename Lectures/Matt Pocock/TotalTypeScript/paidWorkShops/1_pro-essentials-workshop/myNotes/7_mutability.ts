4360f228851b4dae91b2e0fa75e55c3f7507c870
Sumeet Sood <sumeet.sood@perforce.com>
2024-11-13 22:21:39 +0530

    Started with Mutability

--- Lectures/Matt Pocock/TotalTypeScript/paidWorkShops/1_pro-essentials-workshop/myNotes/7_mutability.ts
+++ Lectures/Matt Pocock/TotalTypeScript/paidWorkShops/1_pro-essentials-workshop/myNotes/7_mutability.ts
@@ -2,4 +2,7 @@
  * 97 - type of literal types
  * 98 - object types are assigned widest type
  * 99 - we can define some proeprty  as readonly. this does not add anything at run time, just at type level.
+ *
+ * 103 - readonlyArray - readonly string[] or ReadonlyArray<string>, it only allows method whch does not modify existing array
+ * 104 - readonly array cannot be assigned to mutable array. but other way arund is possible
  */
