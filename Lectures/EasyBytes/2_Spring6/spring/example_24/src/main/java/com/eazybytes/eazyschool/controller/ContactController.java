f6befbc5f5c405b000489ba7db13f5d2109d1163
Sumeet Sood <sumeet.sood@perforce.com>
2025-07-13 23:15:43 +0530

    Completed Section 10

--- Lectures/EasyBytes/2_Spring6/spring/example_24/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ Lectures/EasyBytes/2_Spring6/spring/example_24/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -26,6 +26,17 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
+    /**
+     * So here we need to let our contact.html knows that there is a bean validation
+     * that we need to perform on top of the data that is provided by the end user.
+     * For the same,we add Model Parameter.
+     *
+     * this way I'm indicating to my thymeleaf that this page will hold  the data belongs
+     * to the Contact object, and any validations that I defined inside this Contact bean
+     *should be performed by the Thymeleaf and Spring MVC whenever the user is submitted
+     * the data.
+     *
+     */
     public String displayContactPage(Model model) {
         model.addAttribute("contact", new Contact());
         return "contact.html";
@@ -42,11 +53,31 @@ public class ContactController {
         return new ModelAndView("redirect:/contact");
     }*/
 
+    /**
+     * Here we want to send same form back to user but with 2 things -
+     *   a)Values filled by user should be as it is
+     *   b)Error should be shown to user
+     *
+     * For First we set the property in model with name contact - @ModelAttribute("contact") and it's value
+     *  is same as value of contact object
+     * Then we tell spring to validate the object against POJO class - Vehicle. @Valid
+     * If errors are present then it popoluates the errors object
+     *
+     * If errors are found we redirect the user to new form -> sent 302 status -> so browser goes to /contact page
+     * Other wise we return same contact form, but now because of model -
+     *    we should previous data - attribute 'contact'
+     *    and errors
+     *
+     */
     @RequestMapping(value = "/saveMsg",method = POST)
-    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors, Model model) {
 
         if(errors.hasErrors()){
             log.error("Contact form validation failed due to : " + errors.toString());
+            model.asMap().forEach((key, value) -> {
+                System.out.println("Key: '" + key );
+                System.out.println("value: '" + value );
+            });
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
