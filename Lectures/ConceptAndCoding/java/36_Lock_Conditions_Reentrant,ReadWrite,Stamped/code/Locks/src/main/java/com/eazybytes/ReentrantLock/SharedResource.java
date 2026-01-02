e7ebea9d221585a22cc066f90b2a5e41e73ff48f
Sumeet Sood <sumeet.sood@perforce.com>
2026-01-02 15:41:02 +0530

    Java Notes

---
+++ Lectures/ConceptAndCoding/java/36_Lock_Conditions_Reentrant,ReadWrite,Stamped/code/Locks/src/main/java/com/eazybytes/ReentrantLock/SharedResource.java
@@ -0,0 +1,22 @@
+package com.eazybytes;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SharedResource {
+
+    boolean isAvailable = false;
+
+    public void producer(ReentrantLock lock) {
+        try {
+            lock.lock();
+            System.out.println("Lock acquired by: " + Thread.currentThread().getName());
+            isAvailable = true;
+            Thread.sleep(4000);
+        }
+        catch (InterruptedException e) {}
+        finally {
+            lock.unlock();
+            System.out.println("Lock released by: " + Thread.currentThread().getName());
+        }
+    }
+}
